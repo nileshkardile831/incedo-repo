c6cf40858958a7a5414ae2b42dcb62bf9ecb7774
root <root@ip-172-31-5-69.us-east-2.compute.internal>
2020-02-11 12:35:28 +0000

    shell script updated

--- infra/shell-script.sh
+++ infra/shell-script.sh
@@ -8,6 +8,8 @@ cd $home_dir
 sudo /usr/bin/terraform init;
 sudo /usr/bin/terraform plan;
 sudo /usr/bin/terraform apply -auto-approve;
+
+sudo /usr/bin/aws eks --region us-east-2 update-kubeconfig --name eks-demo
 sudo /home/ec2-user/istio-install/istio-1.4.3/bin/istioctl manifest apply --set profile=demo
 
 sudo /usr/bin/kubectl apply -f ./metrics-server-0.3.6/deploy/1.8+/
