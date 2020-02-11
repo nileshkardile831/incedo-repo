3bae8b196e92b68f5bc91542f639cc28fa549ef4
root <root@ip-172-31-5-69.us-east-2.compute.internal>
2020-02-11 09:19:26 +0000

    terraform apply updated

--- infra/shell-script.sh
+++ infra/shell-script.sh
@@ -8,3 +8,4 @@ cd $home_dir
 sudo /usr/bin/terraform init;
 sudo /usr/bin/terraform plan;
 sudo /usr/bin/terraform apply -auto-approve;
+/home/ec2-user/istio-install/istio-1.4.3/bin/istioctl manifest apply --set profile=demo
