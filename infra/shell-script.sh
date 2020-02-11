a0065af44d6d671b53c6d449900716a1d2337b0a
root <root@ip-172-31-5-69.us-east-2.compute.internal>
2020-02-11 09:25:46 +0000

    terraform apply updated

--- infra/shell-script.sh
+++ infra/shell-script.sh
@@ -8,4 +8,10 @@ cd $home_dir
 sudo /usr/bin/terraform init;
 sudo /usr/bin/terraform plan;
 sudo /usr/bin/terraform apply -auto-approve;
-/home/ec2-user/istio-install/istio-1.4.3/bin/istioctl manifest apply --set profile=demo
+sudo /home/ec2-user/istio-install/istio-1.4.3/bin/istioctl manifest apply --set profile=demo
+
+cd ../third_party_framework
+sudo /usr/bin/kubectl apply -f grafana.yaml
+sudo /usr/bin/kubectl apply -f kaili.yaml
+sudo /usr/bin/kubectl apply -f prometheus.yaml
+sudo /usr/bin/kubectl apply -f tracing.yaml
