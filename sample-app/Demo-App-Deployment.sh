ecea0e139d8cdb2c820fa9b8fae609f2ad2ad907
root <root@ip-172-31-5-69.us-east-2.compute.internal>
2020-02-13 07:33:59 +0000

    added shell script for this job

---
+++ sample-app/Demo-App-Deployment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script is used to provision aws resources using terraform.
+
+set -xv
+
+home_dir=/var/lib/jenkins/workspace/Demo-App-Deployment/sample-app
+cd $home_dir
+
+
+sudo /usr/bin/kubectl apply -f bookinfo/platform/kube/bookinfo.yaml
+sudo /usr/bin/kubectl apply -f bookinfo/networking/bookinfo-gateway.yaml
