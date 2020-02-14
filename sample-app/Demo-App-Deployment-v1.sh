6e45fec8818fa654368f203fd6d0511a4d34c0c2
sandeep2suri <60914874+sandeep2suri@users.noreply.github.com>
2020-02-14 18:40:12 +0530

    Update Demo-App-Deployment-v1.sh
    Updated script to provision istio virtual services for demo app

--- sample-app/Demo-App-Deployment-v1.sh
+++ sample-app/Demo-App-Deployment-v1.sh
@@ -6,6 +6,10 @@ set -xv
 home_dir=/var/lib/jenkins/workspace/Demo-App-Deployment/sample-app
 cd $home_dir
 
-
+# create service and deployment for bookinfo
 sudo /usr/bin/kubectl apply -f bookinfo/platform/kube/bookinfo.yaml
+
+# create virtual service, gateway and destination rule for bookinfo
+sudo /usr/bin/kubectl apply -f bookinfo/networking/virtual-service-all-v1.yaml
 sudo /usr/bin/kubectl apply -f bookinfo/networking/bookinfo-gateway.yaml
+sudo /usr/bin/kubectl apply -f bookinfo/networking/destination-rule-demo-app.yaml
