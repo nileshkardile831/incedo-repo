413549f528262c1662cb4227ff06db399db4c458
sandeep2suri <60914874+sandeep2suri@users.noreply.github.com>
2020-02-14 18:55:58 +0530

    Update Demo-App-Deployment-v2.sh
    updated script file for demo app version two support

--- sample-app/Demo-App-Deployment-v2.sh
+++ sample-app/Demo-App-Deployment-v2.sh
@@ -6,6 +6,9 @@ set -xv
 home_dir=/var/lib/jenkins/workspace/Demo-App-Deployment/sample-app
 cd $home_dir
 
-
+# provision version two of bookinfo app (k8 deployment and service)
 sudo /usr/bin/kubectl apply -f bookinfo/platform/kube/bookinfo_v2.yaml
-sudo /usr/bin/kubectl apply -f bookinfo/networking/bookinfo-gateway.yaml
+
+# add support for version 2 in virtual service and destination rule along with request weight
+sudo /usr/bin/kubectl apply -f bookinfo/networking/virtual-service-demo-app-v2.yaml
+sudo /usr/bin/kubectl apply -f bookinfo/networking/destination-rule-demo-app-v2.yaml
